bf39d9a63af34826d609c859d76df75daaed73df
XSectorz <panatschool@gmail.com>
2024-01-12 20:44:41 +0700

    Fix carousel banner not show correctly

--- vristo-react-main/src/pages/Manages/BannerCarouselObj.tsx
+++ vristo-react-main/src/pages/Manages/BannerCarouselObj.tsx
@@ -14,16 +14,6 @@ const BannerCarouselObj = () => {
     return (
         <div>
             <div className='flex w-full'>
-                <div className='flex justify-center items-center w-40 -translate-x-40 absolute bg-[#666666] text-white' style={{ zIndex: "10" }}>
-                    <div className='flex flex-col justify-center items-center my-1'>
-                        <div className='flex '>
-                            แบนเนอร์
-                        </div>
-                        <div className='flex '>
-                            Carousel
-                        </div>
-                    </div>
-                </div>
                 <Swiper
                     modules={[Autoplay]}
                     autoplay={{ delay: 2000 }}
