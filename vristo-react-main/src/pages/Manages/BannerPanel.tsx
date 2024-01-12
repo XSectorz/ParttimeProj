532ecfe15b0d25ac62704f452d14e9a073d1f38f
XSectorz <panatschool@gmail.com>
2024-01-12 16:15:14 +0700

    Added panel description depend on mode

--- vristo-react-main/src/pages/Manages/BannerPanel.tsx
+++ vristo-react-main/src/pages/Manages/BannerPanel.tsx
@@ -3,7 +3,7 @@ import IconPlus from '../../components/Icon/IconPlus';
 import IconX from '../../components/Icon/IconX';
 import ImagePlaceholder from './ImagePlaceholder';
 import PerfectScrollbar from 'react-perfect-scrollbar';
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import Swal from 'sweetalert2';
 
 interface BannerPanelProps {
@@ -11,8 +11,37 @@ interface BannerPanelProps {
     setCurrentMode: (mode: string) => void;
 }
 
+interface InfoDataItem {
+    title_th: string;
+    title_eng: string;
+    description: string;
+  }
   const BannerPanel: React.FC<BannerPanelProps> = ({ currentMode,setCurrentMode }) => {
+    
     const [imagePlaceholders, setImagePlaceholders] = useState([1]);
+    const [infoDescriptionData, setInfoDescriptionData] = useState<InfoDataItem | null>(null);
+
+    const infoData = [
+        {
+            title_th: "แบนเนอร์",
+            title_eng: "Carousel",
+            description: "แนะนำโปรโมชั่นที่ดีที่สุดหรือแสดงสินค้าและหมวดหมู่สินค้ายอดนิยมในร้านคุณด้วยแบนเนอร์ที่ลูกค้าสามารถเลื่อนดูได้"
+        },
+        {
+            title_th: "หมวดหมู่",
+            title_eng: "Categories",
+            description: "จัดเรียงหมวดหมู่รายการสินค้าเพื่อแนะนำสินค้าให้กับลูกค้าสามารถเลื่อนดูได้"
+        },
+    ]
+
+    useEffect(() => {
+        // ตรวจสอบเมื่อ currentMode เปลี่ยนแปลง
+        if (currentMode === 'categories') {
+            setInfoDescriptionData(infoData[1]);
+        } else {
+            setInfoDescriptionData(infoData[0]);
+        }
+      }, [currentMode]);
 
     const addImagePlaceholder = () => {
         if (imagePlaceholders.length < 4) {
@@ -43,15 +72,15 @@ interface BannerPanelProps {
                 <div className="flex flex-col px-4 w-full bg-[#FFFFFF] pb-4">
                     <div className="flex flex-row justify-between w-full mt-6">
                         <div className="flex items-center justify-center ">
-                            <p className='text-lg font-nunito font-extrabold'>แบนเนอร์&nbsp;</p>
-                            <p className='text-lg font-nunito font-bold'>Carousel</p>
+                            <p className='text-lg font-nunito font-extrabold mr-2'>{infoDescriptionData?.title_th}</p>
+                            <p className='text-lg font-nunito font-bold'>{infoDescriptionData?.title_eng}</p>
                         </div>
                         <button className="flex" onClick={() => setCurrentMode("")}>
                             <IconX className="w-5 h-5" />
                         </button>
                     </div>
                     <div className='flex font-nunito text-sm text-[#8d8d8f] break-words mt-4 mb-2'>
-                        แนะนำโปรโมชั่นที่ดีที่สุดหรือแสดงสินค้าและหมวดหมู่สินค้ายอดนิยมในร้านคุณด้วยแบนเนอร์ที่ลูกค้าสามารถเลื่อนดูได้ ${currentMode}
+                        {infoDescriptionData?.description}
                     </div>
                     <div className='flex w-full h-[3px] bg-[#8d8d8f] mt-2' />
                     <div className='flex flex-col w-full px-4 mt-4'>
